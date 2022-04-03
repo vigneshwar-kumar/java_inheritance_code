364f0ab17f827d2aa652c9bdf4ead1bdb299f0a1
vigneshwar-kumar <100610122+vigneshwar-kumar@users.noreply.github.com>
2022-04-03 10:13:59 +0530

    Single_Inheritance

--- src/Single_Inheritance.java
+++ src/Single_Inheritance.java
@@ -1,12 +1,35 @@
-public class Single_Inheritance extends one{
-    String SecondWord = "World";
+import java.util.Scanner;
+
+public class Single_Inheritance {
+
     public static void main(String[] args) {
-        Single_Inheritance t = new Single_Inheritance();
-        System.out.println(t.FirstWord + t.SecondWord);
+        studentdetail sd = new studentdetail(); // class two
+        sd.getdata(); // getting the input from student class
+        sd.putdata(); // printing the output from student detail class
     }
 }
 
-class one
-{
- String FirstWord = "Hello";
+// class one
+class student {
+    Scanner sc = new Scanner(System.in);
+    int rollno;
+    String name;
+
+    void getdata()
+    {
+        System.out.println("Enter Roll Number : ");
+        rollno = sc.nextInt();
+
+        System.out.println("Enter Name : ");
+        name = sc.next();
+    }
+}
+
+// class two extends with class one
+class studentdetail extends student{
+    void putdata()
+    {        
+        System.out.println("Roll Number : " +rollno);
+        System.out.println("Name : " +name);
+    }
 }
